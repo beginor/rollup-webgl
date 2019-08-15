79fa4b01cf630fedc86239a2c1a739def055f3f1
beginor <beginor@qq.com>
2019-08-15 10:32:10 +0800

    add gl-matrix and matrix transform.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,3 +1,5 @@
+import { mat4, vec3 } from 'gl-matrix';
+
 export class App {
 
     private gl: WebGLRenderingContext;
@@ -19,144 +21,21 @@ export class App {
         this.gl = canvas.getContext('webgl')
             || canvas.getContext('experimental-webgl');
         this.glInfoNode = document.getElementById('glInfo');
-        const drawMode = document.getElementById('drawMode') as HTMLSelectElement;
-        drawMode.addEventListener('change', (e) => {
-            const mode = drawMode.value;
-            this.changeDrawMode(mode);
-        }, false);
         //
         this.glProgram = this.makeProgram();
-        this.createTrianglesBuffer();
+        this.createTriangleBuffer();
         this.render(0);
     }
 
-    private createTrianglesBuffer() {
+    private createTriangleBuffer() {
         const triangle = [
-            -0.5, 0.5, 0.0,
+            0.0, 0.5, 0.0,
             -0.5, -0.5, 0.0,
             0.5, -0.5, 0.0,
-
-            -0.5, 0.5, 0.0,
-            0.5, -0.5, 0.0,
-            0.5, 0.5, 0.0,
         ];
         this.buffer = this.createBuffer(triangle);
-        this.vertexCount = 6;
+        this.vertexCount = 3;
         this.drawMode = this.gl.TRIANGLES;
-    }
-
-    private createTriangleStripBuffer() {
-        const triangleStrip = [
-            -0.5, -0.5, 0.0, // v1
-            -0.5, 0.5, 0.0,  // v3
-            0.5, -0.5, 0.0, // v2
-            0.5, 0.5, 0.0, // v4
-        ];
-        this.buffer = this.createBuffer(triangleStrip);
-        this.vertexCount = 4;
-        this.drawMode = this.gl.TRIANGLE_STRIP;
-    }
-
-    private createTriangleFanBuffer() {
-        const triangleFan = [];
-        triangleFan.push(0);
-        triangleFan.push(0);
-        triangleFan.push(0);
-        let vertexCount = 1;
-        for (let angle = 0; angle <= 280; angle++) {
-            const x = Math.cos(angle / 180 * 3.14);
-            const y = Math.sin(angle / 180 * 3.14);
-            const z = 0.0;
-            triangleFan.push(x);
-            triangleFan.push(y);
-            triangleFan.push(z);
-            vertexCount++;
-        }
-        this.buffer = this.createBuffer(triangleFan);
-        this.vertexCount = vertexCount;
-        this.drawMode = this.gl.TRIANGLE_FAN;
-    }
-
-    private createLinesBuffer() {
-        const lines = [
-            -0.5, -0.5, 0.0,
-            -0.5, 0.5, 0.0,
-
-            -0.5, 0.5, 0.0,
-            0.5, 0.5, 0.0,
-
-            0.5, 0.5, 0.0,
-            0.5, -0.5, 0.0,
-
-            0.5, -0.5, 0.0,
-            -0.5, -0.5, 0.0
-        ];
-        this.buffer = this.createBuffer(lines);
-        this.vertexCount = 8;
-        this.drawMode = this.gl.LINES;
-    }
-
-    private createLineStripBuffer() {
-        const linestrip = [
-            -0.5, -0.5, 0.0,
-            -0.5, 0.5, 0.0,
-            0.5, 0.5, 0.0,
-            0.5, -0.5, 0.0,
-            -0.5, -0.5, 0.0
-        ];
-        this.buffer = this.createBuffer(linestrip);
-        this.vertexCount = 5;
-        this.drawMode = this.gl.LINE_STRIP;
-    }
-
-    private createLineLoopBuffer() {
-        const lineLoop = [
-            -0.5, -0.5, 0.0,
-            -0.5, 0.5, 0.0,
-            0.5, 0.5, 0.0,
-            0.5, -0.5, 0.0
-        ];
-        this.buffer = this.createBuffer(lineLoop);
-        this.vertexCount = 4;
-        this.drawMode = this.gl.LINE_LOOP;
-    }
-
-    private createPointsBuffer() {
-        const points = [
-            -0.5, -0.5, 0.0,
-            -0.5, 0.5, 0.0,
-            0.5, 0.5, 0.0,
-            0.5, -0.5, 0.0
-        ];
-        this.buffer = this.createBuffer(points);
-        this.vertexCount = 4;
-        this.drawMode = this.gl.POINTS;
-    }
-
-    private changeDrawMode(mode: string): void {
-        switch (mode) {
-            case 'triangles':
-                this.createTrianglesBuffer();
-                break;
-            case 'triangle_strip':
-                this.createTriangleStripBuffer();
-                break;
-            case 'triangle_fan':
-                this.createTriangleFanBuffer();
-                break;
-            case 'lines':
-                this.createLinesBuffer();
-                break;
-            case 'line_strip':
-                this.createLineStripBuffer();
-                break;
-            case 'line_loop':
-                this.createLineLoopBuffer();
-                break;
-            case 'points':
-                this.createPointsBuffer();
-                break;
-        }
     }
 
     private render(elapsedTime: number): void {
@@ -196,6 +75,24 @@ export class App {
         this.gl.vertexAttribPointer(position, 3, this.gl.FLOAT, false, 4 * 3, 0);
         const elapsedTimeLoc = this.gl.getUniformLocation(this.glProgram, 'elapsedTime');
         this.gl.uniform1f(elapsedTimeLoc, elapsedTime);
+        // rotate with time
+        const rotateMatrix = mat4.create();
+        mat4.rotate(rotateMatrix, rotateMatrix, elapsedTime / 1000.0, vec3.fromValues(1, 1, 1));
+        // scale with time;
+        const scale = 0.1 * (Math.sin(elapsedTime / 1000.0) + 1.0) + 0.4;
+        const scaleMatrix = mat4.create();
+        mat4.scale(scaleMatrix, scaleMatrix, vec3.fromValues(scale, scale, scale));
+        // offset x with time;
+        const xoffset = Math.sin(elapsedTime / 1000.0);
+        const translateMatrix = mat4.create();
+        mat4.translate(translateMatrix, translateMatrix, vec3.fromValues(xoffset, 0, 0));
+        // final transform
+        const transform = mat4.create();
+        mat4.multiply(transform, rotateMatrix, scaleMatrix);
+        mat4.multiply(transform, transform, translateMatrix);
+        // set transform to shader.
+        const transformUniformLoc = this.gl.getUniformLocation(this.glProgram, 'transform');
+        this.gl.uniformMatrix4fv(transformUniformLoc, false, transform);
         //
         this.gl.drawArrays(this.drawMode, 0, this.vertexCount);
     }
@@ -206,12 +103,10 @@ export class App {
             attribute vec4 position;
             varying vec4 fragColor;
             uniform float elapsedTime;
+            uniform mat4 transform;
             void main() {
                 fragColor = position * 0.5 + 0.5;
-                float rotateAngle = elapsedTime * 0.001;
-                float x = position.x * cos(rotateAngle) - position.y * sin(rotateAngle);
-                float y = position.x * sin(rotateAngle) + position.y * cos(rotateAngle);
-                gl_Position = vec4(x, y, 0.0, 1.0);
+                gl_Position = transform * position;
                 gl_PointSize = 4.0;
             }
         `;
