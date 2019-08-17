29916fb212aa189f6117dcaeee34a37fd53558a6
beginor <beginor@qq.com>
2019-08-17 16:08:05 +0800

    cube & fix canvas viewport

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,17 +3,16 @@ import { mat4, vec3 } from 'gl-matrix';
 export class App {
 
     private gl: WebGLRenderingContext;
-    private glNode: HTMLCanvasElement;
-    private glInfoNode: HTMLElement;
+    private canvasEl: HTMLCanvasElement;
+    private infoEl: HTMLElement;
 
-    private glProgram: WebGLProgram;
+    private program: WebGLProgram;
     private buffer: WebGLBuffer;
     private vertexCount: number;
     private drawMode: number;
-    private perspectiveProjectionMatrix: mat4;
+    private projectionMatrix: mat4;
     private cameraMatrix: mat4;
-    private model1Matrix: mat4;
-    private model2Matrix: mat4;
+    private modelMatrix: mat4;
 
     private lastRenderTime = 0;
     private collectedFrameDuration = 0;
@@ -23,15 +22,26 @@ export class App {
 
     public run(): void {
         const canvas = document.getElementById('glCanvas') as HTMLCanvasElement;
-        this.glNode = canvas;
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+        this.canvasEl = canvas;
         this.gl = canvas.getContext('webgl')
             || canvas.getContext('experimental-webgl');
-        this.glInfoNode = document.getElementById('glInfo');
+        this.resizeCanvas(null);
+        this.infoEl = document.getElementById('glInfo');
         //
-        this.glProgram = this.makeProgram();
+        this.program = this.makeProgram();
         this.createTriangleBuffer();
         this.createMatrix();
         this.render(0);
+        addEventListener('resize', e => this.resizeCanvas(e));
+    }
+
+    private resizeCanvas(e: Event): void {
+        if (!this.gl) {
+            return;
+        }
+        this.gl.viewport(0, 0, this.canvasEl.width, this.canvasEl.height);
     }
 
     private createMatrix(): void {
@@ -39,11 +49,11 @@ export class App {
         mat4.perspective(
             projection,
             80.0 / 180 * Math.PI,
-            this.glNode.width / this.glNode.height,
+            this.canvasEl.width / this.canvasEl.height,
             0.1,
             1000
         );
-        this.perspectiveProjectionMatrix = projection;
+        this.projectionMatrix = projection;
         const camera = mat4.create();
         mat4.lookAt(
             camera,
@@ -52,21 +62,53 @@ export class App {
             vec3.fromValues(0, 1, 0)
         );
         this.cameraMatrix = camera;
-        this.model1Matrix = mat4.create();
-        this.model2Matrix = mat4.create();
+        this.modelMatrix = mat4.create();
     }
 
     private createTriangleBuffer(): void {
         const triangle = [
-            -0.5, 0.5, 0.0,
-            -0.5, -0.5, 0.0,
-            0.5, -0.5, 0.0,
-            -0.5, 0.5, 0.0,
-            0.5, -0.5, 0.0,
-            0.5, 0.5, 0.0
+            // Z轴上的平面
+            -0.5,   0.5,    0.5,
+            -0.5,   -0.5,   0.5,
+            0.5,    -0.5,   0.5,
+            0.5,    -0.5,   0.5,
+            0.5,    0.5,    0.5,
+            -0.5,   0.5,    0.5,
+            -0.5,   0.5,    -0.5,
+            -0.5,   -0.5,   -0.5,
+            0.5,    -0.5,   -0.5,
+            0.5,    -0.5,   -0.5,
+            0.5,    0.5,    -0.5,
+            -0.5,   0.5,    -0.5,
+            // X轴上的平面
+            0.5,    -0.5,   0.5,
+            0.5,    -0.5,   -0.5,
+            0.5,    0.5,    -0.5,
+            0.5,    0.5,    -0.5,
+            0.5,    0.5,    0.5,
+            0.5,    -0.5,   0.5,
+            -0.5,   -0.5,   0.5,
+            -0.5,   -0.5,   -0.5,
+            -0.5,   0.5,    -0.5,
+            -0.5,   0.5,    -0.5,
+            -0.5,   0.5,    0.5,
+            -0.5,   -0.5,   0.5,
+            // Y轴上的平面
+            -0.5,   0.5,    0.5,
+            -0.5,   0.5,    -0.5,
+            0.5,    0.5,    -0.5,
+            0.5,    0.5,    -0.5,
+            0.5,    0.5,    0.5,
+            -0.5,   0.5,    0.5,
+            -0.5,   -0.5,   0.5,
+            -0.5,   -0.5,   -0.5,
+            0.5,    -0.5,   -0.5,
+            0.5,    -0.5,   -0.5,
+            0.5,    -0.5,   0.5,
+            -0.5,   -0.5,   0.5
         ];
         this.buffer = this.createBuffer(triangle);
-        this.vertexCount = 6;
+        this.vertexCount = 6 * 6;
         this.drawMode = this.gl.TRIANGLES;
     }
 
@@ -86,7 +128,7 @@ export class App {
                 1000.0 * this.collectedFrameCount / this.collectedFrameDuration
             );
             if (isNaN(fps) == false || fps < 1000) {
-                this.glInfoNode.innerHTML = "FPS: " + fps;
+                this.infoEl.innerHTML = "FPS: " + fps;
             }
             this.collectedFrameCount = 0;
             this.collectedFrameDuration = 0;
@@ -96,27 +138,18 @@ export class App {
     private draw(deltaTime: number, elapsedTime: number): void {
         // this.gl.viewport(0, 0, this.glCanvasNode.width, this.glCanvasNode.height);
         this.gl.clearColor(1.0, 0.0, 0.0, 1.0);
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        this.gl.enable(this.gl.DEPTH_TEST);
         //
-        this.gl.useProgram(this.glProgram);
+        this.gl.useProgram(this.program);
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
         const position = this.gl.getAttribLocation(
-            this.glProgram, 'position'
+            this.program, 'position'
         );
         this.gl.enableVertexAttribArray(position);
         this.gl.vertexAttribPointer(position, 3, this.gl.FLOAT, false, 4 * 3, 0);
-        const elapsedTimeLoc = this.gl.getUniformLocation(this.glProgram, 'elapsedTime');
+        const elapsedTimeLoc = this.gl.getUniformLocation(this.program, 'elapsedTime');
         this.gl.uniform1f(elapsedTimeLoc, elapsedTime);
-        // // rotate with time
-        // const rotateMatrix = mat4.create();
-        // mat4.rotate(rotateMatrix, rotateMatrix, elapsedTime / 1000.0, vec3.fromValues(0, 1, 0));
-        // // translate matrix
-        // var translateMatrix = mat4.create();
-        // mat4.translate(translateMatrix, translateMatrix, vec3.fromValues(0, 0, -1));
-        // // final transform
-        // const transform = mat4.create();
-        // mat4.multiply(transform, translateMatrix, rotateMatrix);
-        // mat4.multiply(transform, this.perspectiveProjectionMatrix, transform);
         const varyingFactor = (Math.sin(elapsedTime / 1000.0) + 1) / 2.0;
         mat4.lookAt(
             this.cameraMatrix,
@@ -130,42 +163,23 @@ export class App {
             rotateMatrix,
             rotateMatrix,
             varyingFactor * Math.PI * 2,
-            vec3.fromValues(0, 1, 0)
+            vec3.fromValues(1, 1, 1)
         );
         let translateMatrix = mat4.create();
         mat4.translate(
             translateMatrix,
             translateMatrix,
-            vec3.fromValues(-0.7, 0, 0)
+            vec3.fromValues(0, 0, -0.5)
         );
-        mat4.multiply(this.model1Matrix, translateMatrix, rotateMatrix);
-        // setup model2
-        rotateMatrix = mat4.create();
-        mat4.rotate(
-            rotateMatrix,
-            rotateMatrix,
-            varyingFactor * Math.PI * 2,
-            vec3.fromValues(0, 0, 1)
-        );
-        translateMatrix = mat4.create();
-        mat4.translate(
-            translateMatrix,
-            translateMatrix,
-            vec3.fromValues(0.7, 0, 0)
-        );
-        mat4.multiply(this.model2Matrix, translateMatrix, rotateMatrix);
+        mat4.multiply(this.modelMatrix, translateMatrix, rotateMatrix);
         // set matrix to shader.
-        const projectionUniformLoc = this.gl.getUniformLocation(this.glProgram, 'projectionMatrix');
-        this.gl.uniformMatrix4fv(projectionUniformLoc, false, this.perspectiveProjectionMatrix);
-        const cameraUniformLoc = this.gl.getUniformLocation(this.glProgram, 'cameraMatrix');
+        const projectionUniformLoc = this.gl.getUniformLocation(this.program, 'projectionMatrix');
+        this.gl.uniformMatrix4fv(projectionUniformLoc, false, this.projectionMatrix);
+        const cameraUniformLoc = this.gl.getUniformLocation(this.program, 'cameraMatrix');
         this.gl.uniformMatrix4fv(cameraUniformLoc, false, this.cameraMatrix);
-        // draw model1
-        let modelUniformLoc = this.gl.getUniformLocation(this.glProgram, 'modelMatrix');
-        this.gl.uniformMatrix4fv(modelUniformLoc, false, this.model1Matrix);
-        this.gl.drawArrays(this.drawMode, 0, this.vertexCount);
-        // draw model2
-        modelUniformLoc = this.gl.getUniformLocation(this.glProgram, 'modelMatrix');
-        this.gl.uniformMatrix4fv(modelUniformLoc, false, this.model2Matrix);
+        // draw model
+        let modelUniformLoc = this.gl.getUniformLocation(this.program, 'modelMatrix');
+        this.gl.uniformMatrix4fv(modelUniformLoc, false, this.modelMatrix);
         this.gl.drawArrays(this.drawMode, 0, this.vertexCount);
     }
 
